cd36cf36fd4c370ebef92f7e69bc93a320410739
Niels Göran Blume <nielsgoeran.blume@embex.de>
2023-12-19 01:08:11 +0100

    mostly working

--- HighLevelAnalyzer.py
+++ HighLevelAnalyzer.py
@@ -1,20 +1,20 @@
 # High Level Analyzer
 # For more information and documentation, please go to https://support.saleae.com/extensions/high-level-analyzer-extensions
 
-from saleae.analyzers import HighLevelAnalyzer, AnalyzerFrame, StringSetting, NumberSetting, ChoicesSetting
+from saleae.analyzers import HighLevelAnalyzer, AnalyzerFrame, ChoicesSetting
 
+# Custom Imports
+from pprint import pprint
 
 # High level analyzers must subclass the HighLevelAnalyzer class.
 class Hla(HighLevelAnalyzer):
     # List of settings that a user can set for this High Level Analyzer.
-    my_string_setting = StringSetting()
-    my_number_setting = NumberSetting(min_value=0, max_value=100)
-    my_choices_setting = ChoicesSetting(choices=('A', 'B'))
+    partFromMsgToPlot = ChoicesSetting(choices=('CPW-DATA', 'CPW-nEnW', 'CPW-CRC6', 'SPW-DATA', 'SPW-nEnW', 'SPW-LC', 'SPW-CRC16'))
 
     # An optional list of types this analyzer produces, providing a way to customize the way frames are displayed in Logic 2.
     result_types = {
-        'mytype': {
-            'format': 'Output type: {{type}}, Input type: {{data.input_type}}'
+        'part': {
+            'format': 'Found: {{data.input_type}} - {{data.value}}'
         }
     }
 
@@ -25,8 +25,7 @@ class Hla(HighLevelAnalyzer):
         Settings can be accessed using the same name used above.
         '''
 
-        print("Settings:", self.my_string_setting,
-              self.my_number_setting, self.my_choices_setting)
+        print("Settings:", self.partFromMsgToPlot)
 
     def decode(self, frame: AnalyzerFrame):
         '''
@@ -34,8 +33,5 @@ class Hla(HighLevelAnalyzer):
 
         The type and data values in `frame` will depend on the input analyzer.
         '''
-
-        # Return the data frame itself
-        return AnalyzerFrame('mytype', frame.start_time, frame.end_time, {
-            'input_type': frame.type
-        })
+        if frame.type == self.partFromMsgToPlot:
+            return AnalyzerFrame('part', frame.start_time, frame.end_time, {'input_type': frame.type, 'value': frame.data['value']})
